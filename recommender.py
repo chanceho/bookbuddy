3a3d6a7a2f6b5a7adaa3dfc389ca4fa429116ff9
Trang Ho <trangho1490@gmail.com>
2025-04-17 20:06:12 +0700

    Local Web Update

--- recommender.py
+++ recommender.py
@@ -19,9 +19,17 @@ from sklearn.neighbors import NearestNeighbors
 from scipy.sparse import csr_matrix
 import boto3
 import io
+import os
 
 # === 2. GLOBAL DATA LOADING ===
-s3 = boto3.client('s3')
+aws_access_key = os.getenv("AWS_ACCESS_KEY_ID")
+aws_secret_key = os.getenv("AWS_SECRET_ACCESS_KEY")
+region = os.getenv("AWS_DEFAULT_REGION")
+
+s3 = boto3.client('s3',
+    aws_access_key_id=aws_access_key,
+    aws_secret_access_key=aws_secret_key,
+    region_name=region)
 
 bucket_name = 'bookbuddy-app'
 
