6b6547a6a2d0bce668f35459d280f37f09430658
Trang Ho <trangho1490@gmail.com>
2025-04-17 21:55:33 +0700

    Update render

--- api.py
+++ api.py
@@ -66,5 +66,5 @@ def get_book(book_id):
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))
+    port = int(os.environ.get('PORT', 8000))
     app.run(host='0.0.0.0', port=port)
